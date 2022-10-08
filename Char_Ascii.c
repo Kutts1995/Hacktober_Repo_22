14e29ff667c20c78990004845d874c7a2fa719cf
Kuttu Mon <115329342+Kutts1995@users.noreply.github.com>
2022-10-08 07:16:26 +0530

    Create Char_Ascii.c

---
+++ Char_Ascii.c
@@ -0,0 +1,12 @@
+#include <stdio.h>      //Header files inclusion
+
+int main()              //Main part
+   {  
+    char c;
+    printf(" Enter a character: ");
+    scanf("%c",&c);  
+   
+    printf(" ASCII value of %c = %d", c, c);
+    
+    return 0;
+   }
